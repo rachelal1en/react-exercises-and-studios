b5590ef2c2931d7299f721a0df85e60b5f50d6ff
Rachel Allen <rqllen@gmail.com>
2024-08-22 18:13:40 -0500

    RAA 22Aug24

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "New Book Releases!";
+   let book1 = "https://www.booklistqueen.com/wp-content/uploads/such-charming-liars-karen-m-mcmanus-200x300.jpg";
+   let book2 = "http://bookshelfthomasville.com/cdn/shop/files/medea.jpg?v=1705687761";
+   let book3 = "https://m.media-amazon.com/images/I/91R34ScmjlL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Such Charming Liars" />
+         <img src={book2} alt="Medea" />
+         <img src={book3} alt="Everyone Who Can Forgive Me is Dead" />
       </div>      
    );
 }
