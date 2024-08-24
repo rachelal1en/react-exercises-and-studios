aed58117d42d7c758ff458b9ef4a76e5b28c0afe
Rachel Allen <rqllen@gmail.com>
2024-08-24 10:28:51 -0500

    RAA 24Aug24

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -1,9 +1,11 @@
 import './styles.css';
 
 function Button() {
-
+   function onLearnMore() {
+      alert("Splish Splash I was Taking a Bath")
+   };
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
